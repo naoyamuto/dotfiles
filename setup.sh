1c1dcfa06e9de8cd873cc0f0865ca19c048a326c
Naoya Muto <naoya.mutou@gmail.com>
2016-11-01 22:55:37 +0900

    Initialize repository

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+ln -sf ~/Development/src/github.com/naoyamuto/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/Development/src/github.com/naoyamuto/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/Development/src/github.com/naoyamuto/dotfiles/.vimrc ~/.vimrc
+
